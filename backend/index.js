62d8e3a2dc8b62223ea8ef6404d1b0a36cc6c0d1
anthonyk2923 <anthony@fprintf.com>
2024-03-22 11:28:33 -0500

    express.urlencoded({ extended: true }

--- backend/index.js
+++ backend/index.js
@@ -16,22 +16,20 @@ process.on('uncaughtException', function (err) {
   console.log('Node NOT Exiting...');
 });
 
-db.connectDb();
+app.use(express.static(path.join(__dirname, '/public'))); //frontend
 
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('api/teachers', (req, res) => {
-  res.json({});
-});
+db.connectDb();
+
 app.use('/api/teachers', require('./routes/teacherRoutes'));
 app.use('/api/ratings', require('./routes/ratingRoutes'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT || 4444, () =>
   console.log(`Listening on port ${process.env.PORT}`.dim.bold)
